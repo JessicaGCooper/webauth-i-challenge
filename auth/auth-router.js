d75774458426f34a8d895eec38e2cbd842912fb7
Jessica Cooper <jessicagcooper@gmail.com>
2019-12-17 16:28:28 -0600

    create restricted-middleware file, put in session configuration in configure-middleware.js, altered api-router.js as needed.

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -29,6 +29,8 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
        if (user && bcrypt.compareSync(password, user.password)) {
+           //save session for client and send back a cookie
+           req.session.user = user;
            res
            .status(200)
            .json({ message: `Welcome ${user.username}!` });
@@ -44,5 +46,21 @@ router.post('/login', (req, res) => {
         .json({ message: "Server error while logging in.", error})
     });
 });
+
+router.get('/logout', (req, res) => {
+    if (req.session) {
+        req.session.destroy(error => {
+            if(error){
+                res
+                .status(500)
+                .json({ message: "You can checkout anytime, but you can never leave!"})
+            } else {
+                res
+                .status
+                .json({ message: "You have successfully logged out." })
+            }
+        })
+    }
+})
 
 module.exports = router
